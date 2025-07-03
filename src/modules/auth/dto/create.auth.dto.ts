37479c201d8ef12866765395548ced5689cb9f02
Nurmuhammad <ashuraliyevnurmuhammad16@gmail.com>
2025-07-04 01:43:33 +0500

    updates

--- src/modules/auth/dto/create.auth.dto.ts
+++ src/modules/auth/dto/create.auth.dto.ts
@@ -1,6 +1,13 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateAuthDto {
+  @ApiProperty({
+    description: 'Foydalanuvchining telefon raqami',
+    example: '+998901234567',
+    minLength: 10,
+    maxLength: 20,
+  })
   @IsString()
   @MaxLength(20)
   @MinLength(10)
--- src/modules/auth/dto/login.phone.dto.ts
+++ src/modules/auth/dto/login.phone.dto.ts
@@ -1,6 +1,13 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginPhoneNumberDto {
+  @ApiProperty({
+    description: 'Foydalanuvchining telefon raqami',
+    example: '+998901234567',
+    minLength: 12,
+    maxLength: 20,
+  })
   @IsString()
   @MaxLength(20)
   @MinLength(12)
